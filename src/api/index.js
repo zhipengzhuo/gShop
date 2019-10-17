a3d65bcd77751a03a122006bc3fd9b9de9068e66
zhipengzhuo <1289708163@qq.com>
2019-10-17 11:03:50 +0800

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+/*
+包含N个接口请求函数的模块
+函数的返回值是： promise
+*/
+
+import ajax from './ajax'
+
+// 1、根据经纬度获取位置详情
+export const reqAddress = (geohash) => ajax(`/position/${geohash}`)
+// 2、获取食品分类列表
+export const reqCategorys = () => ajax('/index_category')
+// 3、根据经纬度获取商铺列表
+export const reqShops = (longitude, latitude) => ajax('/shops', {longitude, latitude})
+// 4、根据经纬度和关键字搜索商铺列表
+export const reqShopsFromKeyword = ({geohash, keyword}) => ajax('/search_shops', {geohash, keyword})
+// 5、获取一次性验证码
+export const reqVerificationCode = () => ajax('/captcha')
+// 6、用户名密码登陆
+export const reqPwdLogin = (name, pwd, captcha) => ajax('/login_pwd', {name, pwd, captcha}, 'POST')
+// 7、发送短信验证码
+export const reqSendCode = (phone) => ajax('/sendcode', {phone})
+// 8、手机号验证码登陆
+export const reqSmsLogin = ({phone, code}) => ajax('/login_sms', {phone, code}, 'POST')
+// 9、根据会话获取用户信息
+export const reqUser = () => ajax('/userinfo')
+// 10、用户登出
+export const reqLogout = () => ajax('/logout')
